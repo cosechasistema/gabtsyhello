be7f53ffff653d195264bc59b832800e024365fb
Christian Eduardo Scolari <cosechasistema@gmail.com>
2020-06-26 21:52:38 -0300

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Header from "../components/header"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header headerText="About Gatsby" />
+      <Header headerText="It's pretty cool" />
+      <Header headerText="About Gatsby" arbitraryPhrase="is arbitrary" />
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
